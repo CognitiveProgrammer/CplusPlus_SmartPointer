46f175974fe21ed16e0b6692562875b886f7910a
CodesBay <13655419+CodesBay@users.noreply.github.com>
2019-01-12 21:47:13 +0530

    Update unique_ptr.cpp

--- Module2_unique_ptr/unique_ptr.cpp
+++ Module2_unique_ptr/unique_ptr.cpp
@@ -1,6 +1,6 @@
 /*
 * MIT Licence
-* Copyright 2017 @9lean
+* Copyright 2017 @CodesBay
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the 
 * Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, 
